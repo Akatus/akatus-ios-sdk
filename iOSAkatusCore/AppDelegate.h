3e5c4c11a9383a0abea421ddd4e6020b11dcc529
ferbass <ferbass@gmail.com>
2014-04-29 14:16:56 -0300

    initial commit

---
+++ iOSAkatusCore/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iOSAkatusCore
+//
+//  Created by Fernando Bass on 4/29/14.
+//  Copyright (c) 2014 Fernando Bass. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iOSAkatusCore/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iOSAkatusCore
+//
+//  Created by Fernando Bass on 4/29/14.
+//  Copyright (c) 2014 Fernando Bass. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
