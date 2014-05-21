66db7da7387f53c94258231c4951030e213cf05a
ferbass <ferbass@gmail.com>
2014-05-21 16:02:32 -0300

    limpando o codigo

--- iOSAkatusCore/MainViewController.m
+++ iOSAkatusCore/MainViewController.m
@@ -35,6 +35,8 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 }
+
+#pragma mark AK
 - (IBAction)login:(id)sender {
     AKCoreUser *user = [AKCoreUser shared];
     
@@ -64,16 +66,5 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
